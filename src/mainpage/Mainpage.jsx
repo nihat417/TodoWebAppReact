14766b495b090d3c82c9dae8fece3b3464f41ecb
nihat417 <akreminihat@gmail.com>
2024-01-19 19:29:37 +0400

    Last Update

--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,48 +1,76 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoCard from "./components/TodoCard";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
 
 export default function Mainpage({ email, setAuthorized }) {
-  const [isModalOpen,setModalOpen] = useState(false);
-  const [modalType,setModalType] = useState("");
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalType, setModalType] = useState('');
+  const [cards, setCards] = useState([]);
+  const [filterCards, setFilterCards] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [title, setTitle] = useState(''); 
+  const [description, setDescription] = useState('');
 
-  const openModal = (e) => {
+  useEffect(() => {
+    setFilterCards(cards.filter((card) => card.author === email));
+  }, [cards, email]);
+
+  const openModal = (e, card) => {
     setModalOpen(true);
     setModalType(e);
+    setSelectedCard(card);
     document.body.style.overflow = 'hidden';
   }
 
   const closeModal = () => {
     setModalOpen(false);
-    setModalType("");
+    setModalType('');
+    setSelectedCard(null);
     document.body.style.overflow = 'auto';
   }
 
-  // const modalHandle = () => {
-  //   if(modalType === "Create"){
-      
-  //   }
-  // }
+  const modalHandle = (newCard) => {
+    if (modalType === 'Create') {
+      console.log('New Card:', newCard);
+      setCards([...cards, newCard]);
+      closeModal();
+    } 
+    else if (modalType === 'Edit') {
+      if (selectedCard) {
+        const updatedCards = cards.map((card) =>
+          card.id === selectedCard.id ? newCard : card
+        );
+        setCards(updatedCards);
+        closeModal();
+      }
+    } else if (modalType === 'Delete') {
+      if (selectedCard) {
+        const updatedCards = cards.filter((card) => card.id !== selectedCard.id);
+        setCards(updatedCards);
+        closeModal();
+      }
+    }
+  };
 
   return (
-    <>
+    <div>
       <Header email={email} setAuthorized={setAuthorized} />
       <div className="flex">
-        <button className="bg-yellow-400 hover:bg-yellow-500 my-[20px] ml-[20px] py-2 px-4 sm:ml-[30px] sm:py-3 sm:px-10 font-bold rounded-[8px]" onClick={()=>openModal("Create")}>
+        <button className="bg-yellow-400 hover:bg-yellow-500 my-[20px] ml-[20px] py-2 px-4 sm:ml-[30px] sm:py-3 sm:px-10 font-bold rounded-[8px]" onClick={() => openModal("Create")}>
           Create card
         </button>
       </div>
-      
-      {isModalOpen && (<Modal closeModal={closeModal} modalType={modalType}/>)}
+
+      {isModalOpen && (<Modal closeModal={closeModal} setTitle={setTitle} modalType={modalType} setDescription={setDescription} modalHandle={modalHandle} email={email} selectedCard={selectedCard} />)}
 
       <div className="flex flex-wrap justify-center">
-        <TodoCard openModal={openModal} />
-        <TodoCard openModal={openModal} />
-        <TodoCard openModal={openModal} />
-        <TodoCard openModal={openModal} />
-        <TodoCard openModal={openModal} />
+        {filterCards.length ? (
+          filterCards.map((card) => (
+            <TodoCard key={card.id} openModal={openModal} card={card} />
+          ))
+        ) : (<p className="text-center col-span-3 mt-10">No Cards Found</p>)}
       </div>
-    </>
+    </div>
   );
 }
--- src/mainpage/components/TodoCard.jsx
+++ src/mainpage/components/TodoCard.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 
-function TodoCard({openModal}) {
+
+function TodoCard({openModal,card}) {
   return (
-    <div className="flex flex-col m-[10px] sm:w-[560px] md:w-1/3 lg:w-[1/4]">
+    <div className="flex flex-col m-[10px] sm:w-[560px] [md:w-1/3] lg:w-[1/4]">
       <div className="bg-gray-100 p-[20px] rounded-[5px]">
-        <h1 className="font-bold text-3xl">Card Title</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <h1 className="font-bold text-3xl">{card.title}</h1>
+        <p>{card.description}</p>
       </div>
       <div className="bg-gray-300 w-[560  px]">
         <div className="flex justify-end">
-            <button onClick={() => {openModal("Edit")}}   className="flex justify-center m-5 bg-yellow-400 hover:bg-yellow-500 rounded-[10px] items-center font-bold px-[20px] py-[10px] sm:px-[30px] sm:py-[20px] mr-0">Edit</button>
-            <button onClick={() => {openModal("Delete")}} className="flex justify-center m-5 bg-yellow-400 hover:bg-yellow-500 rounded-[10px] items-center font-bold px-[20px] py-[10px] sm:px-[30px] sm:py-[20px]">Delete</button>
+            <button onClick={() => {openModal("Edit",card)}}   className="flex justify-center m-5 bg-yellow-400 hover:bg-yellow-500 rounded-[10px] items-center font-bold px-[20px] py-[10px] sm:px-[30px] sm:py-[20px] mr-0">Edit</button>
+            <button onClick={() => {openModal("Delete",card)}} className="flex justify-center m-5 bg-yellow-400 hover:bg-yellow-500 rounded-[10px] items-center font-bold px-[20px] py-[10px] sm:px-[30px] sm:py-[20px]">Delete</button>
         </div>
       </div>
     </div>
