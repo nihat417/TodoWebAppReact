e5eed93c21f6b610fdb03e96e529f65e9a4eaeec
nihat417 <akreminihat@gmail.com>
2024-01-27 23:38:23 +0400

    Last Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useContext } from "react";
+import { Route,Routes,Navigate } from "react-router-dom";
 import Context from "./ContextWrapper";
 import Mainpage from "./mainpage/Mainpage";
 import Login from "./login/Login";
+import NotFound from "./ErrorPages/NotFound";
 
 export default function App() {
   const { authorized } = useContext(Context);
 
-  return authorized ? <Mainpage/> : <Login/>;
-  
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={"/login"} replace/>}></Route>
+      {
+        authorized ? (<Route path="/mainpage" element={<Mainpage/>}/>) : (<Route path="/login" element={<Login/>}/>)
+      }
+
+      <Route path='*' element={<NotFound/>}/>
+
+    </Routes>
+  )
 }
